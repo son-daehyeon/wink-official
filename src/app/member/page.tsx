1dbec700bbcf05b1e20498544794e856e1ea78c4
Son Daehyeon <sondaehyeon01@gmail.com>
2024-05-28 16:39:30 +0900

    chore: Member페이지 Profile 컴포넌트 분리

--- src/app/member/page.tsx
+++ src/app/member/page.tsx
@@ -2,58 +2,11 @@
 
 import Image from "next/image";
 
-import UserInfo from "@/interfaces/UserInfo"
+import Profile from "@/components/member/Profile";
 
 import cloud from "@/../public/cloud_3d.png";
-import icon_profile from "@/../public/profile.png";
 
 import styles from "@/styles/Member.module.css";
-
-const Profile = ({ name, intro, github, instagram, blog, profile }: UserInfo) => {
-  const websiteList = [
-    [github, "GITHUB"],
-    [instagram, "INSTAGRAM"],
-    [blog, "BLOG"],
-  ];
-
-  return (
-    <div>
-      <div className="flex gap-5 w-[340px] h-[128px] p-5 border-[1.5px] border-[#9DB8FF] rounded-t-lg items-center">
-        <Image
-          className="rounded-full border border-gray-300 bg-[#B0C6FF]"
-          width={80}
-          height={80}
-          src={profile ? `${github}.png` : icon_profile}
-          alt={`${name}'s profile image`}
-        />
-        <div className="flex flex-col gap-1 justify-center">
-          <h1 className="font-pretendard text-xl font-bold">{name}</h1>
-          <h2 className="font-pretendard text-lg">{intro}</h2>
-        </div>
-      </div>
-      <div className="flex w-[340px] h-[58px] pl-5 pr-7 items-center justify-between border-[1.5px] border-[#9DB8FF] rounded-b-lg border-t-0">
-        {websiteList.map((info, index) => {
-          return (
-            <div key={index}>
-              {info[0] !== "" ? (
-                <a
-                  className="font-roboto italic text-[#3A70FF] font-black"
-                  target="_blank"
-                  href={info[0].toString()}
-                  rel="noreferrer"
-                >
-                  {info[1]}
-                </a>
-              ) : (
-                <p className="font-roboto italic text-[#B6CDFF] font-black cursor-default">{info[1]}</p>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
 
 export default function Member() {
   return (
@@ -76,17 +29,17 @@ export default function Member() {
         <div />
       </div>
       <div className="grid justify-center gap-7 px-10 py-2.5 mb-[110px] grid-cols-[repeat(1,_340px)] 2xl:grid-cols-[repeat(4,_340px)] xl:grid-cols-[repeat(3,_340px)] md:grid-cols-[repeat(2,_340px)]">
-        {dataJson.member.map((member, index) => (
-          <Profile
-            key={index}
-            name={member.name}
-            intro={member.intro}
-            github={member.github}
-            instagram={member.instagram}
-            blog={member.blog}
-            profile={member.profile}
-          />
-        ))}
+        {/*{dataJson.member.map((member, index) => (*/}
+        {/*  <Profile*/}
+        {/*    key={index}*/}
+        {/*    name={member.name}*/}
+        {/*    intro={member.intro}*/}
+        {/*    github={member.github}*/}
+        {/*    instagram={member.instagram}*/}
+        {/*    blog={member.blog}*/}
+        {/*    profile={member.profile}*/}
+        {/*  />*/}
+        {/*))}*/}
       </div>
     </>
   );
---
+++ src/components/member/Profile.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+
+import UserInfo from "@/interfaces/UserInfo";
+
+import icon_profile from "../../../public/profile.png";
+
+export default function Profile({ name, intro, github, instagram, blog, profile }: UserInfo) {
+  const websiteList = [
+    [github, "GITHUB"],
+    [instagram, "INSTAGRAM"],
+    [blog, "BLOG"],
+  ];
+
+  return (
+    <div>
+      <div className="flex gap-5 w-[340px] h-[128px] p-5 border-[1.5px] border-[#9DB8FF] rounded-t-lg items-center">
+        <Image
+          className="rounded-full border border-gray-300 bg-[#B0C6FF]"
+          width={80}
+          height={80}
+          src={profile ? `${github}.png` : icon_profile}
+          alt={`${name}'s profile image`}
+        />
+        <div className="flex flex-col gap-1 justify-center">
+          <h1 className="font-pretendard text-xl font-bold">{name}</h1>
+          <h2 className="font-pretendard text-lg">{intro}</h2>
+        </div>
+      </div>
+      <div className="flex w-[340px] h-[58px] pl-5 pr-7 items-center justify-between border-[1.5px] border-[#9DB8FF] rounded-b-lg border-t-0">
+        {websiteList.map((info, index) => {
+          return (
+            <div key={index}>
+              {info[0] !== "" ? (
+                <a
+                  className="font-roboto italic text-[#3A70FF] font-black"
+                  target="_blank"
+                  href={info[0].toString()}
+                  rel="noreferrer"
+                >
+                  {info[1]}
+                </a>
+              ) : (
+                <p className="font-roboto italic text-[#B6CDFF] font-black cursor-default">{info[1]}</p>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
